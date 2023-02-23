db5f84a43490de3e4ff48b4f595ca3be80678388
Nyamburahh <nyamburamary301@gmail.com>
2023-02-23 12:09:31 -0800

    hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point of the program
  *
- * Description: This function prints "_putchar" to the console
- *              using the _putchar function.
+ * Description: This function prints the word "_putchar" to the console,
+ *              one character at a time, using the _putchar function.
  *
  * Return: Always 0 (Success)
  */
